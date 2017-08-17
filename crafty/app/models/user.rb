0951f6019f2ed757c77190cfdbd9702d1b322286
machen2 <victoriamachen@gmail.com>
2017-08-17 20:40:19 +0000

    added user model validations

--- crafty/app/models/user.rb
+++ crafty/app/models/user.rb
@@ -2,4 +2,8 @@ class User < ApplicationRecord
   has_many :crafts
 
   has_secure_password
+
+  validates :name, presence: true, length: { minimum: 2 }
+  validates :email, presence: true, format: { with: /@/, message: "address is invalid." }, uniqueness: true
+  validates :password, length: { minimum: 6 }
 end
