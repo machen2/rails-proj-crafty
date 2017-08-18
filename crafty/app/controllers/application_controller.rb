60932f41dd20f861e96d0f38738d0f834c1ad5f7
machen2 <victoriamachen@gmail.com>
2017-08-18 04:15:15 +0000

    added helper methods for application controller

--- crafty/app/controllers/application_controller.rb
+++ crafty/app/controllers/application_controller.rb
@@ -1,3 +1,17 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  helper_method :current_user, :is_logged_in?, :authenticate_user!
+
+  private
+    def current_user
+      @current_user ||= User.find_by(id: session[:user_id])
+    end
+
+    def is_logged_in?
+      !!current_user
+    end
+
+    def authenticate_user!
+      redirect_to root_path if !is_logged_in? ##CHANGE TO LOGIN PATH
+    end
 end
--- crafty/app/controllers/crafts_controller.rb
+++ crafty/app/controllers/crafts_controller.rb
@@ -1,4 +1,6 @@
 class CraftsController < ApplicationController
+  before_action :authenticate_user!
+
   def index
   end
 end
