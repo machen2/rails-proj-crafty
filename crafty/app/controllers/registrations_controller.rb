5adcd1dffe0aa3261a78ab2b263999101b7b2ad5
machen2 <victoriamachen@gmail.com>
2017-08-18 01:50:16 +0000

    added user params to registrations controller

--- crafty/app/controllers/registrations_controller.rb
+++ crafty/app/controllers/registrations_controller.rb
@@ -5,9 +5,17 @@ class RegistrationsController < ApplicationController
   end
 
   def create
-    @user = User.new
+    @user = User.new(user_params)
     if @user.save
       session[:user_id] = @user.id
       redirect_to crafts_path
+    else
+      render signup_path
+    end
   end
+
+  private
+    def user_params
+      params.require(:user).permit(:name, :email, :password)
+    end
 end
