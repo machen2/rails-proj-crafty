46b2620036312cb2878d51d8a696afd88b30bd95
machen2 <victoriamachen@gmail.com>
2017-08-18 13:20:43 +0000

    added session new and create for login and the view for login, currently buggy

--- crafty/app/controllers/sessions_controller.rb
+++ crafty/app/controllers/sessions_controller.rb
@@ -1,3 +1,14 @@
 class SessionsController < ApplicationController
+  def new
+  end
 
+  def create
+    @user = User.find_by(email: params[:email])
+    if @user && @user.authenticate(params[:password])
+      session[:user_id] = @user.id
+      redirect_to crafts_path
+    else
+      render "sessions/new"
+    end
+  end
 end
--- crafty/app/models/user.rb
+++ crafty/app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   has_secure_password
 
   validates :name, presence: true, length: { minimum: 2 }
-  validates :email, presence: true, format: { with: /@/, message: "address is invalid." }
+  validates :email, presence: true, format: { with: /@/, message: "address is invalid" }
   validates :password, length: { minimum: 6 }
   validates_uniqueness_of :email
 end
