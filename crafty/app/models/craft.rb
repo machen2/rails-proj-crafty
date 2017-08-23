ff189ee8f84c5f8fb64e194ca32c8e5c22ea0002
machen2 <victoriamachen@gmail.com>
2017-08-23 06:45:50 +0000

    added custom attribute writer for tags

--- crafty/app/models/craft.rb
+++ crafty/app/models/craft.rb
@@ -3,8 +3,13 @@ class Craft < ApplicationRecord
   has_many :craft_tags
   has_many :tags, through: :craft_tags
 
-  accepts_nested_attributes_for :tags, reject_if: proc { |attributes| attributes['name'].blank? } ## for temporary use, need to use custom attribute writer
-
   validates_presence_of :title, :difficulty, :description, :instructions
   validates :difficulty, inclusion: { in: %w(easy medium hard), message: "%{value} is not a valid difficulty" }
+
+  def tags_attributes=(tags_attributes)
+    tags_attributes.values.each do |tag_attributes|
+     tag = Tag.find_or_create_by(tag_attributes)
+     tags << tag if tag.persisted? && !tag.name.blank?
+   end
+  end
 end
