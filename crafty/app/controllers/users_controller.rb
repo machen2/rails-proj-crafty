bb97cb4f32519d948a3306ba87c7c770e0c1d895
machen2 <victoriamachen@gmail.com>
2017-08-24 23:41:35 +0000

    changed it so users can view crafts by author, fixed links

--- crafty/app/controllers/users_controller.rb
+++ crafty/app/controllers/users_controller.rb
@@ -3,10 +3,6 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find_by(id: params[:id])
-    if current_user.id == @user.id
-      @ordered_crafts = current_user.crafts.sort_by { |f| f['title'].upcase }
-    else
-      redirect_to crafts_path, alert: "You do not have access to that page."
-    end
+    @ordered_crafts = @user.crafts.sort_by { |f| f['title'].upcase }
   end
 end
