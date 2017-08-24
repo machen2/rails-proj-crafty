6f58d5e9a55d84a28405c5026fd9bb9e4fbde1d2
machen2 <victoriamachen@gmail.com>
2017-08-24 05:17:48 +0000

    added quantity next to tags on index page, ordered tags alphabetically

--- crafty/app/controllers/tags_controller.rb
+++ crafty/app/controllers/tags_controller.rb
@@ -1,6 +1,9 @@
 class TagsController < ApplicationController
+  before_action :authenticate_user!
+
   def index
     @tags = Tag.all
+    @ordered = @tags.sort_by { |f| f['name'].downcase }
   end
 
   def show
