28ea3011c276fc3f56709d089598542e2344cb8b
machen2 <victoriamachen@gmail.com>
2017-08-24 07:39:40 +0000

    added flash alert message for users show redirect

--- crafty/app/controllers/users_controller.rb
+++ crafty/app/controllers/users_controller.rb
@@ -6,7 +6,7 @@ class UsersController < ApplicationController
     if current_user.id == @user.id
       @ordered_crafts = current_user.crafts.sort_by { |f| f['title'].upcase }
     else
-      redirect_to crafts_path
+      redirect_to crafts_path, alert: "You do not have access to that page."
     end
   end
 end
